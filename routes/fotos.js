4b317a8786ce5e8a44c2311a0df341b757c46627
natalie <ntrodriguezch@hotmail.com>
2025-03-13 11:11:45 -0500

    segunda entrega

--- routes/fotos.js
+++ routes/fotos.js
@@ -3,11 +3,17 @@ var router = express.Router();
 
 const Sequelize = require('sequelize');
 const Foto = require('../models').foto;
+const Etiqueta = require('../models').etiqueta;
 
 router.get('/findAll/json',function (req, res, next) {
 
     Foto.findAll({
-        attributes: {exclude:["updatedAt"]}
+        attributes: {exclude:["updatedAt"]},
+        include: [{
+            model: Etiqueta,
+            attributes: ['texto'],
+            through: {attributes: []}
+            }],
     })
     .then(fotos => {
         res.json(fotos);
@@ -19,10 +25,15 @@ router.get('/findAll/json',function (req, res, next) {
 router.get('/findAll/view', function(req, res, next) {
 
     Foto.findAll({
-        attributes: { exclude: ["updatedAt"] }
+        attributes: { exclude: ["updatedAt"] },
+        include: [{
+            model: Etiqueta,
+            attributes: ['texto'],
+            through: {attributes: []}
+            }],
     })
     .then(fotos => {
-        res.render('fotos', { title: 'Fotos', arrFotos: fotos });
+        res.render('fotos', { title: 'Fotos', arrFotos: fotos});
     })
     .catch(error => res.status(400).send(error))
     
---
+++ seeders/20250313150153-fotoetiqueta.js
@@ -0,0 +1,73 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    let [fotos, fotos_metadata] = await queryInterface.sequelize.query('SELECT id FROM fotos')
+    let [etiquetas, etiquetas_metadata] = await queryInterface.sequelize.query('SELECT id FROM etiquetas')
+    await queryInterface.bulkInsert('fotoetiquetas', [
+      { foto_id: fotos[1].id, etiqueta_id: etiquetas[3].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[1].id, etiqueta_id: etiquetas[7].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[2].id, etiqueta_id: etiquetas[1].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[2].id, etiqueta_id: etiquetas[5].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[3].id, etiqueta_id: etiquetas[2].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[3].id, etiqueta_id: etiquetas[6].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[3].id, etiqueta_id: etiquetas[4].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[4].id, etiqueta_id: etiquetas[0].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[5].id, etiqueta_id: etiquetas[4].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[5].id, etiqueta_id: etiquetas[2].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[6].id, etiqueta_id: etiquetas[5].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[6].id, etiqueta_id: etiquetas[3].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[7].id, etiqueta_id: etiquetas[7].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[7].id, etiqueta_id: etiquetas[0].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[8].id, etiqueta_id: etiquetas[1].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[8].id, etiqueta_id: etiquetas[6].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[8].id, etiqueta_id: etiquetas[3].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[9].id, etiqueta_id: etiquetas[2].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[10].id, etiqueta_id: etiquetas[5].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[10].id, etiqueta_id: etiquetas[4].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[11].id, etiqueta_id: etiquetas[3].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[11].id, etiqueta_id: etiquetas[7].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[12].id, etiqueta_id: etiquetas[0].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[12].id, etiqueta_id: etiquetas[1].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[13].id, etiqueta_id: etiquetas[6].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[14].id, etiqueta_id: etiquetas[4].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[14].id, etiqueta_id: etiquetas[3].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[14].id, etiqueta_id: etiquetas[2].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[15].id, etiqueta_id: etiquetas[1].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[16].id, etiqueta_id: etiquetas[5].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[16].id, etiqueta_id: etiquetas[7].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[17].id, etiqueta_id: etiquetas[0].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[17].id, etiqueta_id: etiquetas[6].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[18].id, etiqueta_id: etiquetas[3].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[19].id, etiqueta_id: etiquetas[2].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[19].id, etiqueta_id: etiquetas[4].id, createdAt: new Date(), updatedAt: new Date() },
+
+      { foto_id: fotos[20].id, etiqueta_id: etiquetas[5].id, createdAt: new Date(), updatedAt: new Date() },
+      { foto_id: fotos[20].id, etiqueta_id: etiquetas[1].id, createdAt: new Date(), updatedAt: new Date() }
+    ], 
+    {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('fotoetiquetas', null, {});
+  }
+};
